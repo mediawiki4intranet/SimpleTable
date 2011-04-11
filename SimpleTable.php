68a9e6b98866209bdc90b2a5ad9471e231932b94
Vitaliy Filippov <vitalif@yourcmc.ru>
2011-04-11 15:17:30 +0000

    Bug 76199 - Allow specifying styles for columns

--- SimpleTable.php
+++ SimpleTable.php
@@ -1,7 +1,9 @@
 <?php
 
-/*
+/**
  * Tabbed Data extension.
+ * (c) Vitaliy Filippov 2011
+ * http://wiki.4intra.net/SimpleTable
  *
  * This extension implements a simple method to display tabular data, with a
  * far simpler markup than the standard Wiki table markup.  The goal is to
@@ -16,54 +18,59 @@
  * saves data in this format.  Place the data inside <tab>...</tab> tags,
  * and set any table parameters inside the opening <tab> tag; eg.:
  *
- *   <tab class=wikitable>
+ * <tab class=wikitable sep=tab>
  *   field1\tfield2\t...
  *   field1\tfield2\t...
- *   </tab>
+ * </tab>
  *
  * Additional parameters allowed in the tag are:
- *   sep        Specify a different separator; see the $separators array.
- *   head       Specify a heading; "head=top" makes the first row a heading,
- *              "head=left" makes the first column a heading, "head=topleft"
- *              does both.
+ *   sep    Specify a different separator; can be one of:
+ *          'tab', 'bar', 'space', 'spaces', 'comma', 'semicolon'
+ *   head   Specify a heading; "head=top" makes the first row a heading,
+ *          "head=left" makes the first column a heading, "head=topleft"
+ *          does both.
+ *   colN   Specify CSS style for each cell of _COLUMN_ number N.
+ *          First column has N=1 (not N=0).
  *
+ * Based @ the original SimpleTable
+ *   http://www.mediawiki.org/wiki/Extension:SimpleTable
+ *   (c) Johan the Ghost
  * Thanks for contributions to:
- *  Smcnaught
- *  Frederik Dohr
+ *   Smcnaught
+ *   Frederik Dohr
  */
 
 $wgExtensionFunctions[] = 'wfSimpleTable';
 $wgExtensionCredits['parserhook'][] = array(
-  'name'=>'SimpleTable',
-  'author'=>'Johan the Ghost',
-  'url'=>'http://www.mediawiki.org/wiki/Extension:SimpleTable',
-  'description'=>'Convert tab-separated or similar data into a Wiki table',
+    'name'        => 'SimpleTable',
+    'author'      => 'Vitaliy Filippov',
+    'url'         => 'http://wiki.4intra.net/SimpleTable',
+    'description' => 'Convert tab-separated or similar data into a Wiki table',
 );
-
 
 /*
  * Setup SimpleTable extension.
  * Sets a parser hook for <tab></tab>.
  */
-function wfSimpleTable() {
+function wfSimpleTable()
+{
     new SimpleTable();
 }
 
-
-class SimpleTable {
-
+class SimpleTable
+{
     /*
      * The permitted separators.  An array of separator style name
      * and preg pattern to match it.
      */
     private $separators = array(
-        'space' => '/ /',
-        'spaces' => '/\s+/',
-        'tab' => '/\t/',
-        'comma' => '/,/',
-        'bar' => '/\|/',
+        'space'     => '/ /',
+        'spaces'    => '/\s+/',
+        'tab'       => '/\t/',
+        'comma'     => '/,/',
+        'semicolon' => '/;/',
+        'bar'       => '/\|/',
     );
-
 
     /*
      * Construct the extension and install it as a parser hook.
@@ -73,29 +80,35 @@ class SimpleTable {
         $wgParser->setHook('tab', array(&$this, 'hookTab'));
     }
 
-
     /*
      * The hook function. Handles <tab></tab>.
      * Receives the table content and <tab> parameters.
      */
-    public function hookTab($tableText, $argv, $parser) {
+    public function hookTab($tableText, $argv, $parser)
+    {
         // The default field separator.
         $sep = 'tab';
 
         // Default value for using table headings.
-        $head = null;
+        $head = NULL;
+
+        // Styles for individual columns
+        $colstyle = array();
 
         // Build the table parameters string from the tag parameters.
         // The 'sep' and 'head' parameters are special, and are handled
         // here, not passed to the table.
         $params = '';
-        foreach (array_keys($argv) as $key) {
+        foreach ($argv as $key => $value)
+        {
             if ($key == 'sep')
-                $sep = $argv[$key];
-            else if ($key == 'head')
-                $head = $argv[$key];
+                $sep = $value;
+            elseif ($key == 'head')
+                $head = $value;
+            elseif (preg_match('/^col([1-9]\d*)$/s', $key, $m))
+                $colstyle[$m[1]-1] = $value;
             else
-                $params .= ' ' . $key . '="' . $argv[$key] . '"';
+                $params .= ' ' . $key . '="' . htmlspecialchars($value) . '"';
         }
 
         if (!array_key_exists($sep, $this->separators))
@@ -120,13 +133,12 @@ class SimpleTable {
             if (trim($line))
             {
                 $line = preg_split($this->separators[$sep], $line);
-                if ($headtop && !$i)
-                    $line = '<th>'.implode('</th><th>', $line).'</th>';
-                elseif ($headleft)
-                    $line = ('<th>'.array_shift($line).'</th>') . ('<td>'.implode('</td><td>', $line).'</td>');
-                else
-                    $line = '<td>'.implode('</td><td>', $line).'</td>';
-                $table .= '<tr>' . $line . "</tr>\n";
+                foreach ($line as $j => &$c)
+                {
+                    $e = $headtop && !$i || $headleft && !$j ? 'th' : 'td';
+                    $c = "<$e".($colstyle[$j] ? " style=\"".htmlspecialchars($colstyle[$j])."\"" : '').">$c</$e>";
+                }
+                $table .= "<tr>" . implode('', $line) . "</tr>\n";
             }
         }
         $html = "<table$params>$table</table>";
